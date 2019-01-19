53c9a07852bf3ff934a3f047132ef509ad7e3df0
Tarek <tbadr@ictcube.com>
2019-01-19 10:18:54 +0200

    add Valid Braces

--- 6-kyu/25-Valid-Braces/main.go
+++ 6-kyu/25-Valid-Braces/main.go
@@ -1,70 +1,58 @@
 // https://www.codewars.com/kata/valid-braces/train/go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func main() {
 	fmt.Println(ValidBraces("{}({})[]"))
-	//	fmt.Println(endBraces("(({[]{()}}[{(())}[]]))"))
+	fmt.Println(ValidBraces("(({[]{()}}[{(())}[]]))"))
 }
 
 // ValidBraces :
 func ValidBraces(str string) bool {
-	l := len(str)
-
-	if l%2 != 0 {
-		return false
+	for strings.Index(str, "[]") != -1 || strings.Index(str, "{}") != -1 || strings.Index(str, "()") != -1 {
+		str = strings.Replace(str, "[]", "", -1)
+		str = strings.Replace(str, "{}", "", -1)
+		str = strings.Replace(str, "()", "", -1)
 	}
-
-	for i := 0; i < l; i += 2 {
-		m, _ := checkBraces(str[i], str[l-i-1])
-		if i >= l/2 {
-			m = true
-		}
-
-		c, _ := checkBraces(str[i], str[i+1])
-		if !m && !c {
-			return false
-		}
-	}
-
-	return true
+	return len(str) == 0
 }
 
-// func validateRangeBraces(str string) bool {
+var m = map[string]string{
+	"{": "}",
+	"(": ")",
+	"[": "]",
+}
 
-// 	return true
-// }
-
-// func endBraces(str string) int {
-// 	for i := 0; i < l; i += 2 {
-// 		c, _ := checkBraces(str[i], str[i+1])
-// 		if !m && !c {
-// 			return false
-// 		}
-// 	}
-// 	return 0
-// }
-
-func checkBraces(a, b byte) (bool, byte) {
-	switch a {
-	case '{':
-		if b != '}' {
-			return false, b
+// ValidBracesB :
+func ValidBracesB(str string) bool {
+	s := make([]string, 0)
+	for _, r := range str {
+		r := string(r)
+		if len(s) > 0 && m[s[len(s)-1]] == r {
+			s = s[:len(s)-1]
+		} else {
+			s = append(s, r)
 		}
-		break
-	case '[':
-		if b != ']' {
-			return false, b
-		}
-		break
-	case '(':
-		if b != ')' {
-			return false, b
-		}
-		break
-	default:
-		return false, 0
 	}
-	return true, 0
+	return len(s) == 0
+}
+
+var re = regexp.MustCompile("\\[\\]|\\(\\)|\\{\\}")
+
+// ValidBracesC :
+func ValidBracesC(str string) bool {
+	length := len(str)
+	for {
+		str = re.ReplaceAllLiteralString(str, "")
+		if length == len(str) {
+			break
+		}
+		length = len(str)
+	}
+	return len(str) == 0
 }
--- 6-kyu/25-Valid-Braces/main_test.go
+++ 6-kyu/25-Valid-Braces/main_test.go
@@ -17,7 +17,6 @@ func TestValidBraces(t *testing.T) {
 		{"[({)](]", false},
 		{"(({[]{()}}[{(())}[]]))", true},
 	}
-	// (({[]{()}}[{(())}[]]))
 
 	for _, tc := range tt {
 		t.Run("", func(t *testing.T) {
@@ -27,4 +26,65 @@ func TestValidBraces(t *testing.T) {
 			}
 		})
 	}
+
+	for _, tc := range tt {
+		t.Run("", func(t *testing.T) {
+			res := ValidBracesB(tc.input)
+			if !reflect.DeepEqual(res, tc.expected) {
+				t.Errorf("expected\n%v\ngot\n%v", tc.expected, res)
+			}
+		})
+	}
+
+	for _, tc := range tt {
+		t.Run("", func(t *testing.T) {
+			res := ValidBracesC(tc.input)
+			if !reflect.DeepEqual(res, tc.expected) {
+				t.Errorf("expected\n%v\ngot\n%v", tc.expected, res)
+			}
+		})
+	}
+
 }
+
+//!+bench
+// go test -v  -bench=.
+// go test -bench . -benchmem
+func BenchmarkValidBraces(b *testing.B) {
+	input := "(({[]{()}}[{(())}[]]))"
+	for index := 0; index < b.N; index++ {
+		ValidBraces(input)
+	}
+}
+
+func BenchmarkValidBracesB(b *testing.B) {
+	input := "(({[]{()}}[{(())}[]]))"
+	for index := 0; index < b.N; index++ {
+		ValidBracesB(input)
+	}
+}
+
+func BenchmarkValidBracesC(b *testing.B) {
+	input := "(({[]{()}}[{(())}[]]))"
+	for index := 0; index < b.N; index++ {
+		ValidBracesC(input)
+	}
+}
+
+/*
+
+goos: linux
+goarch: amd64
+BenchmarkValidBraces-8           1000000              1201 ns/op             176 B/op         14 allocs/op
+BenchmarkValidBracesB-8          2000000               943 ns/op             240 B/op          4 allocs/op
+
+
+you can find that Compile regular expression out side 'ValidBracesC' func made a major change in the performance.
+
+@ func with Compile regular expression
+BenchmarkValidBracesC-8           200000              6264 ns/op             240 B/op         19 allocs/op
+
+@ func without Compile regular expression
+BenchmarkValidBracesC-8           100000             22491 ns/op           40240 B/op         50 allocs/op
+
+*/
